4f3f95694ad5e9750438096f326c5ecf9f99c1e9
小熊维尼 <hava a good day>
2022-01-01 19:04:04 +0800

    update

---
+++ src/main/java/cn/itcast/client/RpcClientManager.java
@@ -0,0 +1,135 @@
+package cn.itcast.client;
+
+import cn.itcast.client.handler.RpcResponseMessageHandler;
+import cn.itcast.message.RpcRequestMessage;
+import cn.itcast.protocol.MessageCodecSharable;
+import cn.itcast.protocol.ProcotolFrameDecoder;
+import cn.itcast.protocol.SequenceIdGenerator;
+import cn.itcast.server.service.HelloService;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.util.concurrent.DefaultPromise;
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.Proxy;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class RpcClientManager {
+
+
+    public static void main(String[] args) {
+        HelloService service = getProxyService(HelloService.class);
+        System.out.println(service.sayHello("zhangsan"));
+//        System.out.println(service.sayHello("lisi"));
+//        System.out.println(service.sayHello("wangwu"));
+    }
+
+    // 创建代理类
+    public static <T> T getProxyService(Class<T> serviceClass) {
+        ClassLoader loader = serviceClass.getClassLoader();
+        Class<?>[] interfaces = new Class[]{serviceClass};
+        //                                                            sayHello  "张三"
+        Object o = Proxy.newProxyInstance(loader, interfaces, (proxy, method, args) -> {
+            // 1. 将方法调用转换为 消息对象
+            int sequenceId = SequenceIdGenerator.nextId();
+            RpcRequestMessage msg = new RpcRequestMessage(
+                    sequenceId,
+                    serviceClass.getName(),
+                    method.getName(),
+                    method.getReturnType(),
+                    method.getParameterTypes(),
+                    args
+            );
+            /*
+             TODO: 2022/1/1 https://www.bilibili.com/video/BV1py4y1E7oA?p=136 ：老师这个代码写的有问题
+              发送调用信息时需要先把sequenceId和promise填充到PROMISES再发送，
+              不然先发送了之后服务器如果端响应太快RpcResponseMessageHandler获取sequenceId对应的promise会拿不到
+              而导致消息丢失。
+              当前线程在RpcResponseMessageHandler执行后把sequenceId和promise填充到PROMISES，
+              RpcResponseMessageHandler因为已经执行所以也不会去为该sequenceId对应的promise设置值
+              从而导致当前线程阻塞 ：promise.await();
+             */
+            // 2. 将消息对象发送出去（不能先发！先把当前消息序号和新的promise保存到PROMISES！！！！！！！）
+//            getChannel().writeAndFlush(msg);
+//            TimeUnit.SECONDS.sleep(3);//模拟线程延迟
+
+            // 3. 准备一个空 Promise 对象，来接收结果             指定 promise 对象异步接收结果线程
+            DefaultPromise<Object> promise = new DefaultPromise<>(getChannel().eventLoop());
+            RpcResponseMessageHandler.PROMISES.put(sequenceId, promise);
+
+            //（后发）
+            getChannel().writeAndFlush(msg);
+            TimeUnit.SECONDS.sleep(3);//模拟线程延迟
+
+            // 2. 将消息对象发送出去
+//            getChannel().writeAndFlush(msg);
+//            TimeUnit.SECONDS.sleep(3);
+//            promise.addListener(future -> {
+//                // 线程
+//            });
+
+            // 4. 等待 promise 结果
+            promise.await();
+            if(promise.isSuccess()) {
+                // 调用正常
+                return promise.getNow();
+            } else {
+                // 调用失败
+                throw new RuntimeException(promise.cause());
+            }
+        });
+        return (T) o;
+    }
+
+    private static Channel channel = null;
+    private static final Object LOCK = new Object();
+
+    // 获取唯一的 channel 对象
+    public static Channel getChannel() {
+        if (channel != null) {
+            return channel;
+        }
+        synchronized (LOCK) { //  t2
+            if (channel != null) { // t1
+                return channel;
+            }
+            initChannel();
+            return channel;
+        }
+    }
+
+    // 初始化 channel 方法
+    private static void initChannel() {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
+        MessageCodecSharable MESSAGE_CODEC = new MessageCodecSharable();
+        RpcResponseMessageHandler RPC_HANDLER = new RpcResponseMessageHandler();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.channel(NioSocketChannel.class);
+        bootstrap.group(group);
+        bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel ch) throws Exception {
+                ch.pipeline().addLast(new ProcotolFrameDecoder());
+                ch.pipeline().addLast(LOGGING_HANDLER);
+                ch.pipeline().addLast(MESSAGE_CODEC);
+                ch.pipeline().addLast(RPC_HANDLER);
+            }
+        });
+        try {
+            channel = bootstrap.connect("localhost", 8080).sync().channel();
+            channel.closeFuture().addListener(future -> {
+                group.shutdownGracefully();
+            });
+        } catch (Exception e) {
+            log.error("client error", e);
+        }
+    }
+}
